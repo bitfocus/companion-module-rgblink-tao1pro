bdaaa2829b7c6f147239e72d02bfefceb5ef80b0
Marcin Gumułka <marcingum@gmail.com>
2024-08-18 22:38:06 +0200

    additional tests

--- src/__tests__/Tao1proApi.test.ts
+++ src/__tests__/Tao1proApi.test.ts
@@ -1,4 +1,4 @@
-import { RGBLinkTAO1ProConnector, SRC_HDMI1 } from '../rgblink_tao1pro_connector.js'
+import { RGBLinkTAO1ProConnector, SRC_HDMI1, SRC_HDMI2, SRC_UVC1, SRC_UVC2 } from '../rgblink_tao1pro_connector.js'
 import { ApiConfig } from 'companion-rgblink-openapi-connector'
 
 const TEST_PORT = 25000
@@ -11,11 +11,23 @@ afterEach(() => {
 	}
 })
 
-test('API properly reead 3.2.18 (preview input) ', async () => {
+test('API properly reads feedback 3.2.18 (preview input) ', async () => {
 	// given connectore setted up with enabled polling
 	api = new RGBLinkTAO1ProConnector(new ApiConfig('localhost', TEST_PORT, false, false))
 
-	api.onDataReceived(Buffer.from('<F0000780000000078>'))
+	api.onDataReceived(Buffer.from('<F0000' + '78' + '00' + '00' + '000078>'))
 	await new Promise((r) => setTimeout(r, 1))
 	expect(api.deviceStatus.previewSourceMainChannel).toEqual(SRC_HDMI1)
+
+	api.onDataReceived(Buffer.from('<F0000' + '78' + '00' + '01' + '000079>'))
+	await new Promise((r) => setTimeout(r, 1))
+	expect(api.deviceStatus.previewSourceMainChannel).toEqual(SRC_HDMI2)
+
+	api.onDataReceived(Buffer.from('<F0000' + '78' + '00' + '02' + '00007A>'))
+	await new Promise((r) => setTimeout(r, 1))
+	expect(api.deviceStatus.previewSourceMainChannel).toEqual(SRC_UVC1)
+
+	api.onDataReceived(Buffer.from('<F0000' + '78' + '00' + '03' + '00007B>'))
+	await new Promise((r) => setTimeout(r, 1))
+	expect(api.deviceStatus.previewSourceMainChannel).toEqual(SRC_UVC2)
 })
