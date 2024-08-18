26df2d13a2c1038a6ba3bb1a6e469ce89db57d74
Marcin Gumułka <marcingum@gmail.com>
2024-08-18 22:30:31 +0200

    fix problem with end of line

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,8 +2,14 @@ module.exports = {
 	extends: './node_modules/@companion-module/tools/eslint/main.cjs',
 	// You can add overrides here
 	rules: {
-		'@typescript-eslint/no-for-in-array': "warn",
-		'@typescript-eslint/promise-function-async':'off'
+		'@typescript-eslint/no-for-in-array': 'warn',
+		'@typescript-eslint/promise-function-async': 'off',
+		'prettier/prettier': [
+			'error',
+			{
+				'endOfLine': 'auto'
+			}
+		],
 	},
-	
+
 }
